a3f48895749da7c3b59be19ae38fd1f89f98fcb7
karishma chavan <89963571+KarishmaCh@users.noreply.github.com>
2023-04-12 20:29:29 +0530

    Assignment Display list of products   add , search and sorting button

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,87 @@
-import { useState } from 'react';
-import { Form, Button, ListGroup, } from 'react-bootstrap';
-import { GroceryItem } from './types';
-import './GroceryList.css';
+import React, { useState } from "react";
+import ProductsList from "./ProductsList";
+import ProductForm from "./ProductForm";
+import { Product } from "./products";
 
-function App() {
-const [groceryItems, setGroceryItems] = useState<GroceryItem[]>([]);
-const [inputValue, setInputValue] = useState<string>('');
+// Import Bootstrap CSS and components
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container } from 'react-bootstrap';
 
-const handleAddItem = () => {
-const newItem: GroceryItem = {
-id: Date.now(),
-name: inputValue,
-};
-setGroceryItems([...groceryItems, newItem]);
-setInputValue('');
-};
+const initialProducts: Product[] = [
+  {
+    id: 1,
+    name: "Product 1",
+    description: "Product 1 description",
+    quantity: 10,
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    description: "Product 2 description",
+    quantity: 20,
+    price: 200,
+  },
+];
 
-const handleRemoveItem = (id: number) => {
-const updatedItems = groceryItems.filter((item) => item.id !== id);
-setGroceryItems(updatedItems);
-};
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleAddClick = () => {
+    setShowModal(true);
+  };
+
+  const handleSaveClick = (name: string, description: string, quantity: number, price: number) => {
+    const newProduct: Product = {
+      id: Date.now(),
+      name: name,
+      description: description,
+      quantity: quantity,
+      price: price
+    };
+    setProducts([...products, newProduct]);
+    setShowModal(false);
+  };
+
 
   return (
     <div >
-      
-      <Form>
-  <Form.Group controlId="formGroceryItem">
-    <Form.Control
-      type="text"
-      placeholder="Enter grocery item"
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-      className="grocery-input"
-    />
-  </Form.Group>
-  <Button variant="primary" onClick={handleAddItem} className="add-button">Add Item</Button>
-</Form>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">My App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
 
-<ListGroup>
-  {groceryItems.map((item) => (
-    <ListGroup.Item key={item.id} className="grocery-item">
-      <div className="item-text">{item.name}</div>
-      <Button variant="danger" className="delete-button" onClick={() => handleRemoveItem(item.id)}>Delete</Button>
-    </ListGroup.Item>
-  ))}
-</ListGroup>
+              </li>
+            </ul>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+
+      <div className="container mt-3 ">
+        <div>
+          <button className="btn btn-primary" onClick={handleAddClick}>
+            Add Product
+          </button>
+          </div>
+       
+        <div className="row">
+
+          <div className="col-md-12">
+            <ProductsList products={products} />
+
+          </div>
+        </div>
+        {showModal && <ProductForm onSave={handleSaveClick} />}
+      </div>
     </div>
   );
-}
+};
+
 export default App;
---
+++ src/ProductsList.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { Product } from "./products";
+
+interface Props {
+  products: Product[];
+}
+
+const ProductsList: React.FC<Props> = ({ products }) => {
+  const [sortedProducts, setSortedProducts] = useState<Product[]>(products);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc"); // new state variable
+
+  const handleAddClick = () => {
+    console.log("Add button clicked");
+
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+
+  const handleSortChange = () => {
+    const sorted = [...products].sort((a, b) =>
+      sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    );
+    setSortedProducts(sorted);
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc"); // toggle sort order
+  };;
+
+  const filteredProducts = sortedProducts.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="button-bar">
+      <div className="row">
+        <div className="col-md-12">
+          <input
+            type="text"
+            className="form-control mt-2 mb-2"
+            placeholder="Search"
+            onChange={handleSearchChange}
+          />
+          <button className="btn btn-primary mr-2" onClick={handleSortChange}>
+            {sortOrder === "asc" ? "Sort ascending" : "Sort descending"}
+
+          </button>
+        </div>
+        <table className="table mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.sort((a, b) =>
+              sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+            ).map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>{product.quantity}</td>
+                <td>{product.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsList;
